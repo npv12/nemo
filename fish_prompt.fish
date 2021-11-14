5e61d5ec4441b5a99db28f3621d77d768874f9c2
Pranav <npv12@iitbbs.ac.in>
2021-11-14 21:25:41 +0530

    Clean up colors
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -15,11 +15,14 @@ function fish_prompt
   set -l dirty    "⨯"
   set -l none     "◦"
 
-  set -l normal_color     (set_color normal)
-  set -l success_color    (set_color $fish_pager_color_progress 2> /dev/null; or set_color cyan)
-  set -l error_color      (set_color $fish_color_error 2> /dev/null; or set_color red --bold)
-  set -l directory_color  (set_color $fish_color_quote 2> /dev/null; or set_color brown)
-  set -l repository_color (set_color $fish_color_cwd 2> /dev/null; or set_color green)
+  set -l normal_color (set_color normal)
+  set -l branch_color (set_color yellow)
+  set -l meta_color (set_color red)
+  set -l symbol_color (set_color blue -o)
+  set -l error_color (set_color red -o)
+  set -l success_color (set_color green -o)
+  set -l directory_color  (set_color yellow)
+  set -l repository_color (set_color green)
 
   if test $last_command_status -eq 0
     echo -n -s $success_color $symbol $normal_color
