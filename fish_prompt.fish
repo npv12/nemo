846dec425c000161fa9761807b1447f89c4f5667
Pranav <npv12@iitbbs.ac.in>
2021-11-14 21:30:30 +0530

    Move directory to right
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,19 +1,18 @@
 function fish_prompt
   set -l last_command_status $status
-  set -l cwd
 
-  if test "$theme_short_path" = 'yes'
-    set cwd (basename (prompt_pwd))
-  else
-    set cwd (prompt_pwd)
-  end
+  # All the symbols used here
 
-  set -l symbol   ""
+  # Arrow symbol
+  set -l symbol   "" 
   set -l ahead    "↑"
   set -l behind   "↓"
   set -l diverged "⥄ "
   set -l dirty    "⨯"
   set -l none     "◦"
+
+
+  # All the colors used here
 
   set -l normal_color (set_color normal)
   set -l branch_color (set_color yellow)
@@ -24,20 +23,22 @@ function fish_prompt
   set -l directory_color  (set_color yellow)
   set -l repository_color (set_color green)
 
+
+  # Sets the symbol green if last command was success
+
   if test $last_command_status -eq 0
     echo -n -s $success_color $symbol $normal_color
   else
     echo -n -s $error_color $symbol $normal_color
   end
 
+
   if git_is_repo
     if test "$theme_short_path" = 'yes'
       set root_folder (command git rev-parse --show-toplevel 2> /dev/null)
       set parent_root_folder (dirname $root_folder)
-      set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
     end
 
-    echo -n -s " " $directory_color $cwd $normal_color
     echo -n -s " on " $repository_color (git_branch_name) $normal_color " "
 
     if git_is_touched
@@ -46,7 +47,6 @@ function fish_prompt
       echo -n -s (git_ahead $ahead $behind $diverged $none)
     end
   else
-    echo -n -s " " $directory_color $cwd $normal_color
   end
 
   echo -n -s " "
--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,5 +1,17 @@
 function fish_right_prompt
-  set_color $fish_color_autosuggestion 2> /dev/null; or set_color 555
-  date "+%H:%M:%S"
-  set_color normal
+  set -l cwd
+    set -l cwd_color (set_color yellow)
+    set -l symbol_color (set_color blue)
+
+    if git_is_repo
+        set root_folder (command git rev-parse --show-toplevel 2> /dev/null)
+        set parent_root_folder (dirname $root_folder)
+        set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
+    else
+        set cwd (prompt_pwd)
+    end
+
+    echo -n -s $cwd_color "$cwd"
+    
+    set_color normal
 end
