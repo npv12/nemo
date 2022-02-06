72810977e1ccda4937beff1bddefebdbed411b9a
Pranav <npv12@iitbbs.ac.in>
2022-02-06 10:09:41 +0530

    chore: Switch to catppuccin colors
    Thanks https://github.com/catppuccin/fish
    
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -4,24 +4,42 @@ function fish_prompt
   # All the symbols used here
 
   # Arrow symbol
-  set -l symbol   "死 " 
+  set -l symbol   "死 "
   set -l ahead    "↑"
   set -l behind   "↓"
   set -l diverged "⥄ "
   set -l dirty    "⨯"
   set -l none     "◦"
 
+  # Catppuccin colour palette
+
+  # --> special
+  set -l foreground dadae8
+  set -l selection 3e4058
+
+  # --> palette
+  set -l teal bee4ed
+  set -l flamingo f2cecf
+  set -l magenta c6aae8
+  set -l pink e5b4e2
+  set -l red e38c8f
+  set -l peach f9c096
+  set -l green b1e3ad
+  set -l yellow ebddaa
+  set -l blue a4b9ef
+  set -l grey 6e6c7e
+
 
   # All the colors used here
 
-  set -l normal_color (set_color normal)
-  set -l branch_color (set_color yellow)
-  set -l meta_color (set_color red)
-  set -l symbol_color (set_color blue -o)
-  set -l error_color (set_color red -o)
-  set -l success_color (set_color green -o)
-  set -l directory_color  (set_color yellow)
-  set -l repository_color (set_color green)
+  set -l normal_color (set_color $foreground)
+  set -l branch_color (set_color $yellow)
+  set -l meta_color (set_color $peach)
+  set -l symbol_color (set_color $blue -o)
+  set -l error_color (set_color $red -o)
+  set -l success_color (set_color $green -o)
+  set -l directory_color  (set_color $flamingo)
+  set -l repository_color (set_color $green)
 
 
   # Sets the symbol green if last command was success
@@ -54,11 +72,9 @@ function fish_prompt
   function sideload --description "Sideload a file effortlessly using adb"
     adb reboot sideload-auto-reboot && adb wait-for-device-sideload && adb sideload $argv[1]
   end
-
   function dl_sideload --description "Download and sideload a file effortlessly using adb"
     set file_name (echo $argv[1] | rev | cut -d '/' -f 1 | rev)
     curl $argv[1] -o $file_name
     sideload $file_name
   end
-
 end
